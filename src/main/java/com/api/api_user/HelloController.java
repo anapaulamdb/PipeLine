16c7a64dd57cda24477f61bd07cabfef1b9b364f
Rodrigo Malkã <2020000825@ifam.edu.br>
2022-12-12 23:07:24 -0400

    batata frita

--- src/main/java/com/api/api_user/HelloController.java
+++ src/main/java/com/api/api_user/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class HelloController {
-    String nome = "Usuário";
+    String nome = "Usuáriob";
 
     @GetMapping(value = "Hello")
     public String Hello()
